545d3eae7c7f06555a38d5c73d680104744ce5ef
Elvis Knapman <elvis@elvisknapman.com>
2019-10-10 17:25:27 -0400

    :pencil2: changed from session to server

--- api/server.js
+++ api/server.js
@@ -33,7 +33,7 @@ const sessConfig = {
   resave: false,
   saveUninitialized: false
 };
-session.use(session(sessConfig));
+server.use(session(sessConfig));
 
 server.get("/api", (req, res) => {
   res.status(200).json({ message: "API up" });
