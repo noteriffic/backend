001abf488b2ca499aebc9f146b866488e7f05681
Elvis Knapman <elvis@elvisknapman.com>
2019-10-11 01:59:20 -0400

    :sparkles: wrote create new note route

--- routes/notesRoutes.js
+++ routes/notesRoutes.js
@@ -1,3 +1,22 @@
 const router = require("express").Router();
 
+// notes helpers
+const Notes = require("../helpers/notesHelpers");
+
+// middleware
+const mw = require("../middleware/validation");
+
+router.post("/create", mw.checkNewNote, async (req, res) => {
+  const newNote = req.body;
+
+  try {
+    const result = await Notes.createNote(newNote);
+    console.log("result", result);
+    res.status(201).json(result);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Server error trying to create new note" });
+  }
+});
+
 module.exports = router;
